88067fbc938405fa91e4db3b18b30fa8960b5ac4
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-11 15:15:37 +0100

    MVP completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new()
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @restaurant.reviews << @review
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "Not a valid category" }
+  validates :category, inclusion: { in: CATEGORIES, message: "Not a valid category" }
 end
